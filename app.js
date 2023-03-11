b661a6d9c2e0c661084ee1c58b9888ecbce67ae4
Azajul-Karim <azajulkarim2021@gmail.com>
2023-03-11 12:49:38 +0600

    scroll button added

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const backToTop = document.querySelector(".scroll-top-btn");
 
+window.addEventListener("scroll", scrollBar);
+
+function scrollBar() {
+  if (window.scrollY > 200) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
